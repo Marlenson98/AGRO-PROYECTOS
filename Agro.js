79fd83f0066e8566f35cb196375473f45862f846
AgroProyectos <ingetelecomunicaciones98@gmail.com>
2026-03-11 11:30:36 -0500

    Add files via upload
    Signed-off-by: AgroProyectos <ingetelecomunicaciones98@gmail.com>

--- Agro.js
+++ Agro.js
@@ -6,26 +6,36 @@
 
     let index = 0;
 
-    function moverSlide(direccion){
+    function moverSlide(){
 
-const slides = document.getElementById("slides");
+    const slides = document.getElementById("slides");
+    const total = slides.children.length;
+
+    index++;
+
+    if(index >= total){
+        index = 0;
+    }
+
+    slides.style.transform = "translateX(-" + index * 100 + "%)";
+}
+
+    setInterval(moverSlide,3000);
+
+    function cambiar(direccion){
+
+    const slides = document.getElementById("slides");
     const total = slides.children.length;
 
     index += direccion;
 
     if(index < 0){
         index = total - 1;
-}
+    }
 
-if(index >= total){
+    if(index >= total){
         index = 0;
+    }
+
+    slides.style.transform = "translateX(-" + index * 100 + "%)";
 }
-
-    slides.style.transform = "translateX(" + (-index * 100) + "%";
-}
-
-    setInterval(function(){
-        moverSlide(1);
-}, 4000);
-
-</script>
